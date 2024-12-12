1a77ef7246cd74523da274913a8a42eadea77e23
cheon40 <rudcjs40@gmail.com>
2024-12-12 10:58:32 +0900

    init project

---
+++ src/main/java/org/example/est_team_project2/EstTeamProject2Application.java
@@ -0,0 +1,13 @@
+package org.example.est_team_project2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstTeamProject2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EstTeamProject2Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/est_team_project2/EstTeamProject2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.est_team_project2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstTeamProject2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
