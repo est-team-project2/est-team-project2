6818026df101e33d84e850087d44ce2ef75370bd
[SeokHwanJeongKR] <[jsver12@gmail.com]>
2024-12-13 17:22:27 +0900

    12.13 - form기반 로그인 - 정석환

---
+++ src/main/java/org/example/est_team_project2/domain/Member.java
@@ -0,0 +1,53 @@
+package org.example.est_team_project2.domain;
+
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.example.est_team_project2.domain.eunm.MemberType;
+import org.example.est_team_project2.domain.eunm.SocialType;
+import org.example.est_team_project2.dto.MemberDto;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@Table(name = "member")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+
+    @Id
+    @Column(name = "user_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    private LocalDateTime createAt = LocalDateTime.now(); // 언제 만들어 졌는지
+
+    private LocalDateTime updateAt; //언제 수정되었는지
+
+    @Column(unique = true)
+    private String email; //아이디 겸 이메일
+
+    @Enumerated(EnumType.STRING)
+    private SocialType socialType = SocialType.FORMBASED;  // 어떤 소셜 타입인지
+
+    private String password; //비밀번호
+
+    @Enumerated(EnumType.STRING) // DB에서 enum이라고 인식 할 수 있어서 스트링으로 받아준다
+    private MemberType role = MemberType.USER;
+
+    private String nickName; // 활동명
+
+    @Builder
+    public Member(String nickName, String password, String email) {
+        this.nickName = nickName;
+        this.password = password;
+        this.email = email;
+
+    }
+
+
+
+}
---
+++ src/main/java/org/example/est_team_project2/domain/eunm/SocialType.java
@@ -0,0 +1,8 @@
+package org.example.est_team_project2.domain.eunm;
+
+public enum SocialType {
+    GOOGLE,
+    KAKAO,
+    NAVER,
+    FORMBASED
+}
