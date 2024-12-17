3e629bef588289ca06b6635581043f585e73b95e
cheon40 <rudcjs40@gmail.com>
2024-12-17 16:17:58 +0900

    12.17 - 중복_파일_제거 - cheon

--- src/main/java/org/example/est_team_project2/config/SecurityConfig.java
+++ src/main/java/org/example/est_team_project2/config/SecurityConfig.java
@@ -1,16 +1,19 @@
 package org.example.est_team_project2.config;
 
-import org.example.est_team_project2.domain.eunm.MemberType;
+import lombok.RequiredArgsConstructor;
+import org.example.est_team_project2.service.member.OauthService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final OauthService oauthService;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -22,7 +25,22 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(csrf -> csrf.disable()) // CSRF 비활성화
-                .oauth2Login(Customizer.withDefaults())
+
+                .sessionManagement(session -> session
+                        .sessionFixation(sessionFixation -> sessionFixation.migrateSession())
+                        .maximumSessions(1)
+                        .maxSessionsPreventsLogin(true)
+                )
+
+                .oauth2Login( oauth2 -> oauth2
+                        .loginPage("/signin")
+                        .defaultSuccessUrl("/", true)
+                        .failureUrl("/signin?error=true")
+                        .userInfoEndpoint(userInfo -> userInfo
+                                .userService(oauthService)
+                        )
+                )
+
                 .formLogin(form -> form
                         .loginPage("/login") // 사용자 정의 로그인 페이지
                         .defaultSuccessUrl("/", true) // 성공 시 이동할 URL
@@ -30,23 +48,23 @@ public class SecurityConfig {
                         .failureUrl("/login") // 실패 시 이동할 URL
                         .permitAll() // 로그인 페이지 접근 허용
                 )
+
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/login")
                         .permitAll() // 로그인 페이지 접근 허용
-
-
-//                        .requestMatchers("/user/**")
-//                        .hasAnyAuthority(MemberType.USER.getAuthority()) // USER 권한 필요
-//                        .requestMatchers("/expert/**")
-//                        .hasAnyAuthority(MemberType.EXPERT.getAuthority(), MemberType.ADMIN.getAuthority()) // EXPERT 또는 ADMIN 권한 필요
-//                        .requestMatchers("/admin/**")
-//                        .hasAnyAuthority(MemberType.ADMIN.getAuthority()) // ADMIN 권한 필요
-
-
+                        .requestMatchers("/profile")
+                        .authenticated()
                         .anyRequest()
                         .permitAll() // 나머지 요청은 모두 허용
                 )
+
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/")
+                        .invalidateHttpSession(true)
+                        .deleteCookies("JSESSIONID")
+                )
+
                 .build();
     }
-
 }
